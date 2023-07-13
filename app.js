02e9416a4e3894337007cbb92e71b9e311481ff3
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-07-13 11:06:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.getElementById('heart-rate').innerText = '72 bpm';
+    document.getElementById('blood-pressure').innerText = '120/80 mmHg';
+    document.getElementById('oxygen-level').innerText = '98%';
+    document.getElementById('body-temperature').innerText = '98.6°F';
+    document.getElementById('cholesterol-level').innerText = '200 mg/dL';
+    document.getElementById('glucose-level').innerText = '100 mg/dL';
+}
